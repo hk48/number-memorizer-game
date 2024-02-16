0e75ea4e57c6055723faaa55361008d68c14506e
hk48 <ypprni@gmail.com>
2024-02-16 10:41:42 -0800

    Added a win counter

--- numbermemorizergame.py
+++ numbermemorizergame.py
@@ -2,13 +2,22 @@ import PySimpleGUI as sg
 import random
 layout = [[sg.Button("GENERATE")], 
           [sg.Input(key='-GUESS-', do_not_clear=False)],
-          [sg.Button("GUESS")]]
+          [sg.Button("GUESS")],
+          [sg.Text('Record:')], [sg.Text("", key='-WINS-')],]
 def regen():
-    gen = str(random.randrange(1000000, 9999999))
+    #to do: let user pass number of digits
+    gen = str(random.randrange(1, 9))
     return gen
 
+wins = "" 
+def winner(s):
+    global wins
+    for ele in s:
+        wins += ele
+    return wins
+
 # Create the window
-window = sg.Window("Number Memorizer Game", layout)
+window = sg.Window("Number Memorizer Game", layout,grab_anywhere=True)
 
 # Create an event loop
 while True:
@@ -20,9 +29,13 @@ while True:
     if event == "GUESS":
         if values["-GUESS-"] == gen:
             sg.popup_timed("Correct")
-            regen()
+            winner("W ")
+            window['-WINS-'].update(wins[::-1])
         else:
-             sg.popup_timed("wrong")
+             sg.popup_timed("Wrong. Number was: ", gen)
+             winner("L ")
+             window['-WINS-'].update(wins[::-1])
+             gen = regen()
     if event == sg.WIN_CLOSED:
         break
 
